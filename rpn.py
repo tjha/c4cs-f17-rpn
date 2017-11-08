4e4f5f9ccac6f4e791329058ebae168ef1539754
tjha <tjha@umich.edu>
2017-11-08 16:03:22 -0500

    Changed files to match what was on c4cs website as final class rpn calculator to start homework

--- rpn.py
+++ rpn.py
@@ -1,27 +1,36 @@
 #!/usr/bin/env python3
 
+import operator
+
+
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+}
+
 def calculate(myarg):
     stack = list()
     for token in myarg.split():
-        if token == '+':
-            arg2  = stack.pop()
-            arg1 = stack.pop()
-            result = arg1 + arg2
-            stack.append(result)
-        elif token == '-':
+        try:
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            function = operators[token]
             arg2 = stack.pop()
             arg1 = stack.pop()
-            result = arg1 - arg2
+            result = function(arg1, arg2)
             stack.append(result)
-        else:
-            stack.append(int(token))
-    print(stack)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
     return stack.pop()
 
 def main():
     while True:
-        calculate(input("rpn calc> "))
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
 
 if __name__ == '__main__':
     main()
-
