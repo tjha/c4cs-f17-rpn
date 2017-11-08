a0470dcd77d135bd3e83fef8476234a1fc5af1d1
tjha <tjha@umich.edu>
2017-11-08 15:56:34 -0500

    Added files for RPN calculator that were completed in class

---
+++ rpn.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
+        if token == '+':
+            arg2  = stack.pop()
+            arg1 = stack.pop()
+            result = arg1 + arg2
+            stack.append(result)
+        elif token == '-':
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = arg1 - arg2
+            stack.append(result)
+        else:
+            stack.append(int(token))
+    print(stack)
+    return stack.pop()
+
+def main():
+    while True:
+        calculate(input("rpn calc> "))
+
+if __name__ == '__main__':
+    main()
+
